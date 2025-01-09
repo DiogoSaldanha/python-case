5b0ab582eae28cff19c3e0481fdd24fe36fbb60e
Diogo Schaan Saldanha <diogo.saldanha@pmgramado.local>
2025-01-09 11:05:49 -0300

    Refactoring code

--- main.py
+++ main.py
@@ -1,72 +1,86 @@
 import requests
 import pandas
 
-# Variáveis para as URL's
+class DataCase:
+    def __init__(self, usersURL, nameOfExcelFile, postEndpoint):
+        self.usersURL = usersURL
+        self.nameOfExcelFile = nameOfExcelFile
+        self.postEndpoint = postEndpoint
+        self.userData = []
+
+    def getUsersFromAPI(self):
+        usersGetResponse = requests.get(self.usersURL)
+        return usersGetResponse.json()
+    
+    def getPostsFromAPI(self, currentUserId):
+        postsGetResponse = requests.get(f"{self.usersURL}/{currentUserId}/posts")
+        return postsGetResponse.json()
+    
+    def calculateAverageOfCharacters(self, posts, amountOfPosts):
+        bodyCharacterByUser = 0
+
+        for post in posts:
+            postBody = post['body']
+            postBodyLength = len(postBody)
+            bodyCharacterByUser += postBodyLength
+
+        averageOfCharacters = bodyCharacterByUser / amountOfPosts
+        return averageOfCharacters
+
+    def processData(self):
+        currentUserID = 1
+        getResponse = self.getUsersFromAPI()
+        
+        for user in getResponse:
+            userId = user['id']
+            userName = user['name']
+
+            print("------------------------------------------------------------------------------------------")
+            print("ID do usuário: ", userId)
+            print("Nome do usuário: ", userName)
+
+            postsResponse = self.getPostsFromAPI(currentUserID)
+            amountOfPosts = len(postsResponse)
+            average = self.calculateAverageOfCharacters(postsResponse, amountOfPosts)
+            currentUserID += 1
+
+            for post in postsResponse:
+                print(f"Título do post de ID {post['id']}: {post['title']}")
+                print(f"Body do post de ID {post['id']}: {post['body']}")
+                print(f"Quantidade de caracteres no body do post: {len(post['body'])}\n")
+
+            print(f"Média de caracteres do user de ID {userId}: {average}")
+
+            self.userData.append({
+                "ID de Usuário": userId,
+                "Nome de Usuário": userName,
+                "Quantidade de Posts": amountOfPosts,
+                "Média de Caracteres por Post": average
+            })
+
+        print("_________________________________________________________________")
+        print("\n\n-------------------------------------------------------\nDados do Usuário:\n-------------------------------------------------------\n", self.userData)
+        print("-------------------------------------------------------\n")
+
+    def createDataFrame(self):
+        dataFrame = pandas.DataFrame(self.userData)
+        dataFrame.to_excel(self.nameOfExcelFile, index=False)
+        print(f"\n-------------------------------------------------------\nDados transferidos para o arquivo {self.nameOfExcelFile} com sucesso.\n-------------------------------------------------------")
+
+    def postToEndpoint(self):
+        endpointPostRequest = requests.post(self.postEndpoint, json=self.userData)
+        print("\n-------------------------------------------------------\nStatus da requisição POST: ", endpointPostRequest.status_code)
+        print("Resposta: \n-------------------------------------------------------", endpointPostRequest.text)
+
+    def runCode(self):
+        self.processData()
+        self.createDataFrame()
+        self.postToEndpoint()
+
+
 usersURL = "https://jsonplaceholder.typicode.com/users"
+excelFileName = "nome-teste.xlsx"
+fakeEndpoint = "https://jsonplaceholder.typicode.com/send-email"
 
-# Respostas GET dos endpoints para 'users'
-usersGetResponse = requests.get(usersURL)
-usersResponse = usersGetResponse.json()
-
-#Printando informações
-currentID = 1
-listOfAverageBody = []
-userData = []
-for item in usersResponse:
-    userId = item['id']
-    userName = item['name']
-
-    print("------------------------------------------------------------------------------------------")
-    print("ID do usuário: ", userId)
-    print("Nome do usuário: ", userName)
-
-    # Resgatando e printando os dados para as requisições dos Posts
-    print('\nPosts:')
-
-    postsURL= f"https://jsonplaceholder.typicode.com/users/{currentID}/posts"
-    postsGetResponse = requests.get(postsURL)
-    postsResponse = postsGetResponse.json()
-    bodyCharactersByUser = 0
-    for id in postsResponse:
-        postTitle = id['title']
-        postBody = id['body']
-        postBodyLength = len(postBody)
-
-        print(f"Título do post de ID {id['id']}: ", postTitle)
-        print(f"Body do post de ID {id['id']}: ", postBody)
-        print("Quantidade de caracteres no body do post: ", postBodyLength)
-        print()
-
-        # Em uma variável antes de incrementar 'currentID', posso ir adicionando os valores para salvá-los em uma lista e depois fazer o cálculo
-        bodyCharactersByUser += postBodyLength
-
-    currentID += 1
-
-    #Percebi que o len de "postsResponse" é igual ao número de posts feitos por user, o que me leva a fazer o cálculo de maneira mais fácil dentro da própria iteração
-    amountOfPosts = len(postsResponse)
-    averageOfCharacters = bodyCharactersByUser / amountOfPosts
-    print(f"Média de caracteres do user de ID {userId}: {averageOfCharacters}")
-
-    userData.append({
-        "ID de Usuário": userId,
-        "Nome de Usuário": userName,
-        "Quantidade de Posts": amountOfPosts,
-        "Média de Caracteres por Post": averageOfCharacters
-    })
-
-
-print("\n", userData)
-
-#Usando pandas para criar a planilha
-
-#Criando o DataFrame
-dataFrame = pandas.DataFrame(userData)
-dataFrame.to_excel("user-data.xlsx", index=False)
-print("\nDados transferidos para o arquivo 'user-data.xlsx' com sucesso.")
-
-#Enviando para endpoint fictício
-endpointURL = "https://jsonplaceholder.typicode.com/send-email"
-postRequest = requests.post(endpointURL, json=userData)
-
-print("\nStatus da requisição POST: ", postRequest.status_code)
-print("Resposta: ", postRequest.text)
+main = DataCase(usersURL, excelFileName, fakeEndpoint)
+main.runCode()
